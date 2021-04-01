46bb792dab7787bf6fa15813c29b8992f69c7d7f
Mary Papamichalis <mary.papamichalis@gmail.com>
2021-03-31 23:57:53 -0500

    second attempt

--- server.js
+++ server.js
@@ -20,19 +20,3 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/powerful-retreat-39538",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  });
-
-require("./routes/index")(app); 
-require("./routes/api")(app); 
-
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
