9848d9d061d6934bd07b3bb76d1685051fa0522b
Mary Papamichalis <mary.papamichalis@gmail.com>
2021-03-31 23:53:03 -0500

    deploying

--- server.js
+++ server.js
@@ -2,8 +2,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config();
 const app = express();
+const logger = require("morgan");
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fitness",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/powerful-retreat-39538",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 require("./routes/index")(app); 
 require("./routes/api")(app); 
